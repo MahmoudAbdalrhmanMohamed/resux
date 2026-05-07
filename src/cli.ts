c755b56dbef45a337d7c4e512d894f79aeefc014
Mahmoud Abdalrahman Mohamed <mahnoudhouda5632@gmail.com>
2026-05-07 16:13:21 +0300

    Fix Nitro adapter response handling and bump to v0.2.26

--- src/cli.ts
+++ src/cli.ts
@@ -957,7 +957,24 @@ function createNitroHandler(): string {
   return `import { fromNodeMiddleware } from "h3";
 import { createResuxNodeHandler } from "resuxjs/node";
 
-export default fromNodeMiddleware(createResuxNodeHandler());
+const resuxHandler = createResuxNodeHandler();
+
+export default fromNodeMiddleware((req, res, next) => {
+  resuxHandler(req, res);
+
+  if (res.writableEnded) {
+    return;
+  }
+
+  const done = () => {
+    res.off("finish", done);
+    res.off("close", done);
+    next();
+  };
+
+  res.once("finish", done);
+  res.once("close", done);
+});
 `;
 }
 
